b62912dc2526e3ae586b3e1f1c3dcf139d7334b4
Katrina Fuller <trinafuller@att.net>
2019-08-27 14:46:15 -0600

    houses are added to the page

--- app/Controllers/HouseController.js
+++ app/Controllers/HouseController.js
@@ -3,10 +3,38 @@ import HouseService from "../Services/HouseService.js";
 //Private
 let _houseService = new HouseService()
 
+function _draw() {
+  let houses = _houseService.Houses
+  let template = ''
+  houses.forEach(h => template += h.Template)
+  document.getElementById('houses-cards').innerHTML = template
+}
 
 //Public
 export default class HouseController {
   constructor() {
     // console.log("hello from house controller")
+
+    //register add subscribers
+    _houseService.addSubscriber('houses', _draw)
+
+    //retrieve data
+    _houseService.getApiHouses()
+  }
+
+  addHouse(e) {
+    e.preventDefault()
+    let form = e.target
+    let data = {
+      imgUrl: form.imgUrl.value,
+      price: form.price.value,
+      year: form.year.value,
+      levels: form.levels.value,
+      bedrooms: form.bedrooms.value,
+      bathrooms: form.bathrooms.value,
+      description: form.description.value
+    }
+    _houseService.addHouse(data)
+    form.reset()
   }
 }
--- app/Services/HouseService.js
+++ app/Services/HouseService.js
@@ -29,6 +29,10 @@ export default class HouseService {
     // console.log("Hello from House Service")
   }
 
+  addSubscriber(propName, fn) {
+    _subscribers[propName].push(fn)
+  }
+
   get Houses() {
     return _state.houses.map(h => new House(h))
   }
@@ -38,6 +42,17 @@ export default class HouseService {
       .then(res => {
         let housesData = res.data.data.map(h => new House(h))
         _setState('houses', housesData)
+      })
+      .catch(err => {
+        console.error(err)
+      })
+  }
+
+  addHouse(data) {
+    _houseApi.post('', data)
+      .then(res => {
+        _state.houses.push(res.data.data)
+        _setState('houses', _state.houses)
       })
       .catch(err => {
         console.error(err)
